526e02384a1172a2f3ce87124273611a1a016269
KesGroom <kesgroom@gmail.com>
2020-06-12 10:29:50 -0500

    Se aplican cambios a la tabla de usuario

--- src/java/Controladores/UsuarioControlador.java
+++ src/java/Controladores/UsuarioControlador.java
@@ -15,8 +15,10 @@ import Facade.UsuarioFacade;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import static java.lang.Integer.getInteger;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import javax.ejb.EJB;
 import javax.faces.context.FacesContext;
 
@@ -28,7 +30,6 @@ import javax.faces.context.FacesContext;
 @SessionScoped
 public class UsuarioControlador implements Serializable {
 
-    
     /**
      * Creates a new instance of usuarioControlador
      */
@@ -45,10 +46,10 @@ public class UsuarioControlador implements Serializable {
 
     @EJB
     UsuarioFacade usuarioFacade;
-    
+
     @EJB
     RolFacade rolFacade;
-    
+
     @EJB
     TipoFacade tipoFacade;
 
@@ -57,7 +58,7 @@ public class UsuarioControlador implements Serializable {
         usuario.setIdTipo(tipoFacade.find(tipo.getIdTipo()));
         usuarioFacade.create(usuario);
         usuario = new Usuario();
-        
+
         return "Usuarios";
     }
 
@@ -71,11 +72,11 @@ public class UsuarioControlador implements Serializable {
     public List<Usuario> consultarUsuarios() {
         return usuarioFacade.consultarUsuario(1);
     }
-    
-    public List<Usuario> buscarRol(int rol){
+
+    public List<Usuario> buscarRol(int rol) {
         return usuarioFacade.busquedaRol(rol);
     }
-    
+
     public Usuario getUsuario() {
         return usuario;
     }
@@ -100,4 +101,26 @@ public class UsuarioControlador implements Serializable {
         this.tipo = tipo;
     }
 
+    public boolean globalFilterFunction(Object value, Object filter, Locale locale) {
+        String filterText = (filter == null) ? null : filter.toString().trim().toLowerCase();
+        if (filterText == null || filterText.equals("")) {
+            return true;
+        }
+        int filterInt = getInteger(filterText);
+
+        Usuario usu = (Usuario) value;
+        return usu.getNumerodeDocumento().toLowerCase().contains(filterText)
+                || usu.getIdTipo().getIdTipo() < filterInt
+                || usu.getIdRoles().getIdRoles() < filterInt
+                || usu.getApellido().toLowerCase().contains(filterText)
+                || usu.getNombre().toLowerCase().contains(filterText)
+                || usu.getCelular().toLowerCase().contains(filterText)
+                || usu.getTelefonoFijo().toLowerCase().contains(filterText)
+                || usu.getCorreoElectronico().toLowerCase().contains(filterText)
+                || usu.getDireccion().toLowerCase().contains(filterText);
+
+    }
+
 }
+
+
